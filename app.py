fbe3b8537547c7c2153f00959f82c484a9c4d25e
Than Thien <50171638+thanthienhai@users.noreply.github.com>
2023-12-18 18:56:12 -0800

    09.55.19.12 update

--- app.py
+++ app.py
@@ -14,11 +14,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Read the CSV file
     csv_file_path = 'data.csv'  # Replace with the path to your CSV file
     df = pd.read_csv(csv_file_path)
-    #menu_items = ['Trang chủ', 'Nhập dữ liệu', 'Phân tích khách hàng', 'Phân tích thời gian', 'Phân tích cửa hàng', 'Dự đoán doanh số', 'Dự đoán doanh số']
-    return render_template('index.html', table=df.to_html(classes='table table-striped', border=0, justify='unset', col_space=0))
+    first_5_rows = df.head(5)
+    thongKe = df.describe()
+    
+    return render_template('index.html', table=first_5_rows.to_html(classes='table table-striped', border=0, justify='unset', col_space=0), table_2=thongKe.to_html(classes='table table-striped', border=0, justify='unset', col_space=0))
 
 #--------------------------------------------
 app.config['SECRET_KEY'] = 'O5y2agwlKXlug2PI92K34MVXnSba96tK'
@@ -65,18 +66,21 @@ def kh_button():
     if isinstance(df, (pd.DatetimeIndex, pd.MultiIndex)):
         df = df.to_frame(index=False)
 
-# remove any pre-existing indices for ease of use in the D-Tale code, but this is not required
+    # remove any pre-existing indices for ease of use in the D-Tale code, but this is not required
     df = df.reset_index().drop('index', axis=1, errors='ignore')
     df.columns = [str(c) for c in df.columns]  # update columns to strings in case they are numbers
 
     chart_data = pd.concat([
         df['Product line'],
-        df['Quantity'],
+        df['Total'],
         df['Gender'],
     ], axis=1)
     chart_data = chart_data.query("""(`Gender` == 'Female') or (`Gender` == 'Male')""")
     chart_data = chart_data.sort_values(['Gender', 'Product line'])
     chart_data = chart_data.rename(columns={'Product line': 'x'})
+    chart_data_sum = chart_data.groupby(['Gender','x'], dropna=True)[['Total']].sum()
+    chart_data_sum.columns = ['Total||sum']
+    chart_data = chart_data_sum.reset_index()
     chart_data = chart_data.dropna()
     # WARNING: This is not taking into account grouping of any kind, please apply filter associated with
     #          the group in question in order to replicate chart. For this we're using '"""`Gender` == 'Female'"""'
@@ -87,17 +91,47 @@ def kh_button():
     charts = []
     charts.append(go.Bar(
         x=chart_data['x'],
-        y=chart_data['Quantity'],
+        y=chart_data['Total||sum'],
         name='(Gender: Female)'
     ))
     figure = go.Figure(data=charts, layout=go.Layout({
         'barmode': 'group',
         'legend': {'orientation': 'h', 'y': -0.3},
-        'title': {'text': '(Gender: Female) - Quantity by Product line'},
+        'title': {'text': '(Gender: Female) - Sum of Total by Product line'},
         'xaxis': {'title': {'text': 'Product line'}},
-        'yaxis': {'tickformat': '0:g', 'title': {'text': 'Quantity'}, 'type': 'linear'}
+        'yaxis': {'title': {'text': 'Sum of Total'}, 'type': 'linear'}
     }))
     chart_div = figure.to_html(full_html=False)
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    '''
+    chart_data2 = pd.concat([
+        df['Customer type'],
+        df['Total'],
+        df['City'],
+    ], axis=1)
+    chart_data2 = chart_data2.query("""(`City` == 'Mandalay') or (`City` == 'Naypyitaw') or (`City` == 'Yangon')""")
+    chart_data2 = chart_data2.sort_values(['City', 'Customer type'])
+    chart_dat2 = chart_data2.rename(columns={'Customer type': 'x'})
+    chart_data2 = chart_data2.dropna()
+    # WARNING: This is not taking into account grouping of any kind, please apply filter associated with
+    #          the group in question in order to replicate chart. For this we're using '"""`City` == 'Mandalay'"""'
+    chart_data2 = chart_data2.query("""`City` == 'Mandalay'""")
+
+    charts2 = []
+    charts2.append(go.Bar(
+        x=chart_data2['x'],
+        y=chart_data2['Total'],
+        name='(City: Mandalay)'
+    ))
+    figure2 = go.Figure(data=charts2, layout=go.Layout({
+        'barmode': 'group',
+        'legend': {'orientation': 'h', 'y': -0.3},
+        'title': {'text': '(City: Mandalay) - Total by Customer type'},
+        'xaxis': {'title': {'text': 'Customer type'}},
+        'yaxis': {'title': {'text': 'Total'}, 'type': 'linear'}
+    }))
+    chart_div2 = figure2.to_html(full_html=False)
+    '''
     return render_template('chart.html', chart_div=chart_div)
     # Thực hiện các hành động khi Button 2 được click
     #return "Button 2 clicked!"
